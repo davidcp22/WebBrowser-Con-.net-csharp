8c8b01c2547e9498a544010c8eb4c02ffb7c1b81
David Castaño <dcastano@audisoftnet.com>
2016-04-08 09:38:23 -0500

    1
    webBrowser

---
+++ navegador_web/navegador_web/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace navegador_web
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cmbFavoritos = new System.Windows.Forms.ComboBox();
+            this.txtDirrecion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtDirrecion);
+            this.panel1.Controls.Add(this.cmbFavoritos);
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.btnAtras);
+            this.panel1.Controls.Add(this.btnIr);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1102, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 89);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1102, 399);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // btnIr
+            // 
+            this.btnIr.Location = new System.Drawing.Point(418, 1);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(75, 23);
+            this.btnIr.TabIndex = 0;
+            this.btnIr.Text = "Ir";
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(499, 1);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 23);
+            this.btnAtras.TabIndex = 1;
+            this.btnAtras.Text = "Atras";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(0, 29);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(156, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregrar a favoritos";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // cmbFavoritos
+            // 
+            this.cmbFavoritos.FormattingEnabled = true;
+            this.cmbFavoritos.Location = new System.Drawing.Point(236, 31);
+            this.cmbFavoritos.Name = "cmbFavoritos";
+            this.cmbFavoritos.Size = new System.Drawing.Size(176, 21);
+            this.cmbFavoritos.TabIndex = 3;
+            this.cmbFavoritos.SelectedIndexChanged += new System.EventHandler(this.cmbFavoritos_SelectedIndexChanged);
+            // 
+            // txtDirrecion
+            // 
+            this.txtDirrecion.Location = new System.Drawing.Point(0, 3);
+            this.txtDirrecion.Name = "txtDirrecion";
+            this.txtDirrecion.Size = new System.Drawing.Size(412, 20);
+            this.txtDirrecion.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FAVORITOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 488);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "BrowserDavid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtDirrecion;
+        private System.Windows.Forms.ComboBox cmbFavoritos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnIr;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ navegador_web/navegador_web/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace navegador_web
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> favoritos = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.google.com.co");
+            txtDirrecion.Text = "www.google.com.co";
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(txtDirrecion.Text);
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+            txtDirrecion.Text = (webBrowser1.Url.ToString());
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            favoritos.Add(webBrowser1.Url.ToString());
+            comboxFavorito();
+        }
+
+        private void comboxFavorito()
+        {
+            cmbFavoritos.Items.Clear();
+            foreach (string dirrecion in favoritos)
+            {
+                cmbFavoritos.Items.Add(dirrecion);
+
+            }
+        }
+
+        private void cmbFavoritos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(cmbFavoritos.SelectedItem.ToString());
+            txtDirrecion.Text = (cmbFavoritos.SelectedItem.ToString());
+
+
+        }
+    }
+}
